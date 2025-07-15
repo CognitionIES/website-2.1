ff1efde400b42a15d9976770a1d7b342ac1025c8
CognitionIES <it_support@cognitionies.com>
2025-07-15 14:35:46 +0530

    Bug Solved

--- app/services/build-operate/about.tsx
+++ app/services/build-operate/about.tsx
@@ -248,7 +248,7 @@ const WhyChooseUs = () => {
               When you choose us, you gain a committed partner who blends
               engineering expertise with the flexibility and scale needed to
               deliver your most ambitious projects. From building dedicated
-              teams to managing every stage end-to-end, we're here to help you
+              teams to managing every stage end-to-end, we&apos;re here to help you
               succeed.
             </motion.p>
           </motion.div>
